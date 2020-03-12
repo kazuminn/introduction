c704ca7c2e0336e7d0da16f78a1a732f9e04b3a5
Kazuminn <warugaki.k.k@gmail.com>
2020-03-13 01:14:52 +0900

    first commit

---
+++ graph-bfs/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+int main(){
+vector<vector<int> > G = {
+    {1,4},
+    {5},
+    {3,5},
+    {6,7},
+    {0},
+    {1,2},
+    {2,3,5},
+    {3,6}, 
+};
+}
